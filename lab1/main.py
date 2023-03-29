778687d08eba4ac886da9bace6ca7ee9d32a6b12
tronyaginaa <tronyaginaa@gmail.com>
2023-03-29 11:28:38 +0300

    add main

---
+++ lab1/main.py
@@ -0,0 +1,11 @@
+import plots
+import tables
+
+
+if __name__ == '__main__':
+    # plots.plot_density()
+    # plots.plot_box_plot()
+    # plots.plot_ecdf()
+    # plots.plot_kde()
+    # tables.print_tables()
+    tables.print_outlin()
